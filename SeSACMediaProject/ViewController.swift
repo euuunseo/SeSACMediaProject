23027816a28888af5338bf9b94a1aa5b188c9443
euuunseo <gjgh1004@gmail.com>
2023-10-31 00:08:54 +0900

    Initial Commit

---
+++ SeSACMediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACMediaProject
+//
+//  Created by 고은서 on 2023/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
